23eece8418fc31c3390e3dcb2f68b2f5c719f6c5
pattnaik_soumitri <soumitri.2.pattnaik@shoppertrak.com>
2022-11-18 14:21:50 +0530

    first commit with the issue

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+<v-app>
+  <v-container>
+
+    <v-select 
+      :items="['Apple', 'Orange']"
+      label="Pick a fruit"
+      v-model="fruit"
+      @change="() => console.log('It Works!')"
+    />
+
+    You have selected: {{fruit}}
+  </v-container>
+</v-app>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+// The v-model
+const fruit = ref();
+
+</script>
